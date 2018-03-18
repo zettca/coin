327f1dc5f9e55c811dbc8d8ecb4420d92ce9129f
Bruno <zettca@gmail.com>
2018-03-18 01:19:45 +0000

    Initial commit

---
+++ src/main/java/ist/sec/coin/App.java
@@ -0,0 +1,13 @@
+package ist.sec.coin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
