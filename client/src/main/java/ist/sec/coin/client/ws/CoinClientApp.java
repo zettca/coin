64991298e2aba866ccf29dc777941524bb75ccd3
Bruno <zettca@gmail.com>
2018-04-09 06:35:42 +0100

    Implement initial client structure

---
+++ client/src/main/java/ist/sec/coin/client/ws/CoinClientApp.java
@@ -0,0 +1,10 @@
+package ist.sec.coin.client.ws;
+
+public class CoinClientApp {
+
+    public static void main(String[] args) {
+        // TODO: connect to server/frontend
+
+        System.out.println("Client launched!");
+    }
+}
---
+++ client/src/test/java/ist/sec/coin/client/TrivialTest.java
@@ -0,0 +1,15 @@
+package ist.sec.coin.client;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TrivialTest {
+    public TrivialTest() {
+    }
+
+    @Test
+    public void trivial() {
+        assertTrue(true);
+    }
+}
