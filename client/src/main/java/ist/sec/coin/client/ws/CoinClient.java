88ad7174a1528caf27ca98130f64ccc004b819ac
Bruno <zettca@gmail.com>
2018-04-11 02:45:49 +0100

    Implement basic client connection

---
+++ client/src/main/java/ist/sec/coin/client/ws/CoinClient.java
@@ -0,0 +1,51 @@
+package ist.sec.coin.client.ws;
+
+import ist.sec.coin.server.ws.AccountAddress;
+import ist.sec.coin.server.ws.CoinService;
+import ist.sec.coin.server.ws.CoinServiceImplService;
+import ist.sec.coin.server.ws.PKey;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class CoinClient implements CoinService {
+    private CoinService port;
+
+    CoinClient() {
+        CoinServiceImplService service = new CoinServiceImplService();
+        port = service.getCoinServiceImplPort();
+    }
+
+    CoinClient(String endpointURL) {
+        this();
+        try {
+            URL endpoint = new URL(endpointURL);
+        } catch (MalformedURLException e) {
+            System.err.println("Malformed endpoint URL: " + endpointURL);
+        }
+    }
+
+    public String echo(String message) {
+        return port.echo(message);
+    }
+
+    public void register(PKey publicKey) {
+        port.register(publicKey);
+    }
+
+    public void sendAmount(AccountAddress source, AccountAddress destination, int amount) {
+        port.sendAmount(source, destination, amount);
+    }
+
+    public void checkAccount(AccountAddress address) {
+        port.checkAccount(address);
+    }
+
+    public void receiveAmount(AccountAddress address) {
+        port.receiveAmount(address);
+    }
+
+    public void audit(AccountAddress address) {
+        port.audit(address);
+    }
+}
--- client/src/main/java/ist/sec/coin/client/ws/CoinClientApp.java
+++ client/src/main/java/ist/sec/coin/client/ws/CoinClientApp.java
@@ -3,8 +3,14 @@ package ist.sec.coin.client.ws;
 public class CoinClientApp {
 
     public static void main(String[] args) {
-        // TODO: connect to server/frontend
+        String endpoint = args[0];
+
+        CoinClient client = new CoinClient(endpoint);
 
         System.out.println("Client launched!");
+
+        String echoResponse = client.echo("BlaBlaAlah!");
+
+        System.out.println("Server responded with: " + echoResponse);
     }
 }
