6a370339183879f2a88f8efa439a26eb9828e052
Bruno <zettca@gmail.com>
2018-05-07 18:06:15 +0100

    Add UDDINaming Implement UDDI server registering

--- server/src/main/java/ist/sec/coin/server/ws/CoinServiceApp.java
+++ server/src/main/java/ist/sec/coin/server/ws/CoinServiceApp.java
@@ -1,22 +1,33 @@
 package ist.sec.coin.server.ws;
 
+import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
+import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINamingException;
+
 import javax.xml.ws.Endpoint;
 import java.security.Security;
 
 public class CoinServiceApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UDDINamingException {
         if (args.length < 1) {
             System.err.println("Missing endpoint URL");
         }
 
-        String endpoint = args[0];
+        String uddiURL = args[0];
+        String endpointURL = args[1];
+        String endpointName = args[2];
 
         System.out.println("Signature algorithms: " + Security.getAlgorithms("Signature"));
         System.out.println("MessageDigest algorithms: " + Security.getAlgorithms("MessageDigest"));
 
-        Endpoint.publish(endpoint, new CoinServiceImpl());
-        System.out.println("Server running at: " + endpoint);
+        System.out.println("Starting server endpoint...");
+        Endpoint.publish(endpointURL, new CoinServiceImpl());
+        System.out.println("Server Endpoint running at: " + endpointURL);
+
+        System.out.println("Publishing server endpoint...");
+        UDDINaming uddiNaming = new UDDINaming(uddiURL);
+        uddiNaming.rebind(endpointName, endpointURL);
+        System.out.println("Server Endpoint published to: " + uddiURL);
     }
 
 }
